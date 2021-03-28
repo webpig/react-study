4fe240f27db4c43a745a6a75bba78f55e5624ecb
朱宝华 <zhubaohua@zhubaohuadeMacBook-Pro.local>
2021-03-28 14:25:36 +0800

    feat: counter-app demo 新增reset、组合组件等功能

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: 1,
-    };
-
-    hanldeIncrement = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-
     render() {
         return (
-            <React.Fragment>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.hanldeIncrement} className="btn btn-secondary btn-sm">Increment</button>
-            </React.Fragment>
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+            </div>
         );
     }
 
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
-        classes += this.state.count === 0 ? 'warning' : 'primary';
+        classes += this.props.counter.value === 0 ? 'warning' : 'primary';
         return classes;
     }
 
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+    formatValue() {
+        const { value } = this.props.counter;
+        return value === 0 ? 'Zero' : value;
     }
 }
  
