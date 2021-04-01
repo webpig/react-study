3fd5eab20c45714cea6df57965d414f57d52cb7a
朱宝华 <zhubaohua@zhubaohuadeMacBook-Pro.local>
2021-04-02 00:45:11 +0800

    feat: 电影demo新增分页功能

---
+++ vidly/src/components/common/pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+const Pagination = (props) => {
+    const { itemsCount, currentPage, pageSize } = props;
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+    if (pagesCount === 1) return null;
+    const pages = _.range(1, pagesCount + 1);
+
+    return (
+        <nav aria-label="Page navigation example">
+            <ul className="pagination">
+                {
+                    pages.map(page => <li onClick={() => props.onPageChange(page)} className={ page === currentPage ? 'page-item active' : 'page-item'} key={page}>
+                        <a className="page-link" href="#">{page}</a>
+                    </li>)
+                }
+            </ul>
+        </nav>
+    );
+}
+
+Pagination.propTypes = {
+    itemsCount: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired
+};
+ 
+export default Pagination;
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
+import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
     state = { 
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4,
+        currentPage: 1
     }
 
     handleDelete = (movie) => {
@@ -23,10 +27,17 @@ class Movies extends Component {
         this.setState({ movies });
     }
 
+    handlePageChange = (page) => {
+        this.setState({ currentPage: page });
+    }
+
     render() {
         const { length: count } = this.state.movies;
+        const { currentPage, pageSize, movies: allMovies } = this.state;
 
         if (count === 0) return <p>There no movies in the database.</p>
+
+        const movies = paginate(allMovies, currentPage, pageSize);
 
         return ( 
             <React.Fragment>
@@ -43,7 +54,7 @@ class Movies extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.movies.map(movie => (
+                        {movies.map(movie => (
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
@@ -63,6 +74,12 @@ class Movies extends Component {
                         ))}
                     </tbody>
                 </table>
+                <Pagination
+                    itemsCount={count}
+                    currentPage={currentPage}
+                    pageSize={pageSize}
+                    onPageChange={this.handlePageChange}
+                />
             </React.Fragment>
         );
     }
