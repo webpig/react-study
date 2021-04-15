88b544d1339cd14cb48cccd4907c657c97039f62
朱宝华 <zhubaohua@zhubaohuadeMacBook-Pro.local>
2021-04-15 23:38:27 +0800

    feat: vidly加入导航和路由信息

---
+++ vidly/src/components/movieForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MovieForm = ({ match, history }) => {
+    return (
+        <React.Fragment>
+            <h1>Movie Form {match.params.id}</h1>
+            <button className="btn btn-primary" onClick={() => history.push('/movies')}>Save</button>
+        </React.Fragment>
+    )
+        
+}
+ 
+export default MovieForm;
---
+++ vidly/src/components/navBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <NavLink className="nav-item nav-link" to="/movies">Movies</NavLink>
+                        <NavLink className="nav-item nav-link" to="/customers">Customers</NavLink>
+                        <NavLink className="nav-item nav-link" to="/rentals">Rentals</NavLink>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
